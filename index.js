d722785075811405d7cc85ae8454a9224a6bb7c4
DEEPAKPREM98 <92307969+DEEPAKPREM98@users.noreply.github.com>
2023-07-28 12:12:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "/")));
 
 
 //HOME PAGE
-app.get('/home', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
